75a67c1bd13d4d2fa2cb14b51aa4c245bad515e3
Gabriel Firmamento <126484540+GabrielFAlves@users.noreply.github.com>
2024-06-19 11:36:19 -0300

    'cry'

--- demo/src/main/java/com/example/entity/Users.java
+++ demo/src/main/java/com/example/entity/Users.java
@@ -2,6 +2,8 @@ package com.example.entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
@@ -10,6 +12,7 @@ import jakarta.persistence.Table;
 public class Users {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private String id;
     private String name;
     
